a505a9823138032fc617d81ee96e383b118183a2
UsamaDaood <mac@macs-MacBook-Pro.local>
2023-01-28 16:12:01 +0500

    Update Commit

--- app/libs/responsiveFont.tsx
+++
@@ -1,8 +0,0 @@
-import { Dimensions } from 'react-native';
-import { RFValue } from 'react-native-responsive-fontsize';
-
-const { height } = Dimensions.get('window');
-
-export const responsiveFontSize = (size: number) => {
-  return RFValue(size, height);
-};
